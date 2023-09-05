c389967f7eea592f0fd1f18656f5a566c3979785
irynaVaku <vakulovskaya@gmail.com>
2023-09-04 19:24:35 -0800

    New tests

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,14 +1,30 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Authentication and Autorization', () => {
-  test('Sign In with valid credentials', async ({page}) => {
-    await page.goto('https://coding.pasv.us/user/login')
+  test('Sign In with valid credentials', async ({ page }) => {
+    await page.goto('https://coding.pasv.us/user/login');
     //await page.pause()
 
-    await page.locator('#normal_login_email').fill('vakulovskaya@gmail.com')
-    await page.locator('#normal_login_password').fill('!QA2ws3ed')
-    await page.locator('button[type="submit"]').click()
+    await page.locator('#normal_login_email').fill('vakulovskaya@gmail.com');
+    await page.locator('#normal_login_password').fill('!QA2ws3ed');
+    await page.locator('button[type="submit"]').click();
 
-    await expect(page.locator('.ant-avatar-square')).toBeVisible()
-  })
+    await expect(page.locator('.ant-avatar-square')).toBeVisible();
   });
+
+  test.only('Sign In with invalid credentials', async ({ page }) => {
+    await page.goto('https://coding.pasv.us/user/login');
+    //await page.pause()
+
+    await page.locator('#normal_login_email').fill('123qwert@gmail.com');
+    await page.locator('#normal_login_password').fill('!QA2ws3ed');
+    await page.locator('button[type="submit"]').click();
+
+    await expect(
+      page.locator('.ant-notification-notice-message')
+    ).toBeVisible();
+    await expect(
+      page.locator('.ant-notification-notice-message')
+    ).toContainText('User login. Fail');
+  });
+});
